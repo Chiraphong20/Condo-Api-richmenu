477ad399e3e98f13144b86cc07ba187002aaaf7d
chiraphong <gamemodo6z@gmail.com>
2025-06-03 23:16:50 +0700

    ppp

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const RICH_MENUS = require('../richmenus');
+const RICH_MENUS = require('./richmenus');
 
 require('dotenv').config();
 const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
