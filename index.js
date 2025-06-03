00c1eb8543acb0e61ba0dfc05ad11f07f600e0a4
chiraphong <gamemodo6z@gmail.com>
2025-06-03 23:28:25 +0700

    asdada

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//index.js
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -11,7 +10,11 @@ app.use(express.json());
 
 app.use('/api/register', registerRoute);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001; // <-- ให้ใช้ env PORT ก่อน
+
+console.log('Starting app...');
+console.log('PORT:', PORT);
+
 app.listen(PORT, () => {
   console.log(`🚀 Backend running on http://localhost:${PORT}`);
 });
